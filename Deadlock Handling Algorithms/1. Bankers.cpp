0851b6cbf0f55f3bdbba128345f6d0f6bfe92936
Mostafa Kamal <mostafakamal.cse2022@gmail.com>
2025-08-05 17:10:37 +0600

    Simple Implementation of Banker's Algorithm

--- Deadlock Handling Algorithms/1. Bankers.cpp
+++ Deadlock Handling Algorithms/1. Bankers.cpp
@@ -1,11 +1,7 @@
 #include <bits/stdc++.h>
 #define int long long
 using namespace std;
-#ifndef ONLINE_JUDGE
-#define dout(...) cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; _print(__VA_ARGS__)
-#else
-#define dout(...)
-#endif
+
 signed main() {
     int _nProcess, _nResource;
     cin >> _nProcess >> _nResource;
