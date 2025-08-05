2897b4e841d9628233cfe6ab8b2a616739b3d09f
Mostafa Kamal <mostafakamal.cse2022@gmail.com>
2025-08-05 23:04:17 +0600

    Best Fit memory Allocation

--- Memory Allocation Algorithms/2. Best Fit.cpp
+++ Memory Allocation Algorithms/2. Best Fit.cpp
@@ -3,6 +3,57 @@
 using namespace std;
 
 signed main() {
-    
+    int _nProcess, _nBlock;
+    cin >> _nBlock >> _nProcess;
+
+    vector<int> Block(_nBlock + 1, 1e18); // We initialize all block size to 1e18
+    for (int i = 0; i < _nBlock; i++) {
+        cin >> Block[i];
+    }
+
+    vector<int> Process(_nProcess);
+    for (int i = 0; i < _nProcess; i++) {
+        cin >> Process[i];
+    }
+
+    vector<int> Allocated(_nProcess, -1), Occupied(_nBlock, 0);
+    for (int i = 0; i < _nProcess; i++) {
+        int id = _nBlock;
+        for (int j = 0; j < _nBlock; j++) {
+            if (Occupied[j] == 0 and Block[j] >= Process[i]) {
+                if (Block[id] > Block[j]) {
+                    id = j;
+                }
+            }
+        }
+        if (id != _nBlock) {
+            Allocated[i] = id;
+            Occupied[id] = 1;
+        }
+    }
+
+    cout << "\nProcess No.\tProcess Size\tBlock no." << endl;
+    for (int i = 0; i < _nProcess; ++i) {
+        cout << "   " << i + 1 << "\t\t" << Process[i] << " \t\t";
+        if (Allocated[i] == -1) {
+            cout << "Not Allocated";
+        } else {
+            cout << Allocated[i] + 1;
+        }
+        cout << endl;
+    }
     return 0;
 }
+
+/*
+Test :
+3 3
+5 10 20
+10 20 5
+
+Expected : 
+Process No.	Process Size	Block no.
+   1		    10 		        2
+   2		    20 		        3
+   3		    5 		        1
+*/
